c3ef18623b79c8acf4f83b811fa68804296d8de5
patricollo <251016@edu.p.lodz.pl>
2024-04-11 21:24:37 +0200

    added subtraction

--- main.cpp
+++ main.cpp
@@ -37,7 +37,9 @@ int main() {
                 printf("%d + %d = %d\n", number1, number2, result);
                 break;
             case 2:
-
+                enterNumbers(&number1, &number2);
+                result = number1 - number2;
+                printf("%d - %d = %d\n", number1, number2, result);
                 break;
             case 3:
 
